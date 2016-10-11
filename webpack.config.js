b7d6473a5ca8f8d1318879368f888700655ee325
TsaiKoga <koga.c@shopperplus.com>
2016-10-11 22:08:21 +0800

    Add webpack production configure file and its plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ var path = require("path");
 
 module.exports = {
   entry: [
-    'webpack/hot/dev-server',
+    'webpack/hot/dev-server',             // 热启动，只有development模式下使用
     path.join(__dirname, "public/javascripts/app.js")
   ],
   output: {
-    path: path.resolve(__dirname, "views"),
-    publicPath: "/assets/",
+    path: path.resolve(__dirname, "public/javascripts"), // output.path 生成的js存储路径
+    publicPath: 'http://localhost:8080/', // output.publicPath:会重写你项目中html的src的前缀，例如<script src='重写前缀'>
     filename: "bundle.js"
   },
   node: {
---
+++ webpack.production.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'public/javascripts/app.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, "public/javascripts"),
+    filename: "bundle.js"
+  },
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),                  // 有些JS库有自己的依赖树，并且这些库可能有交叉的依赖，DedupePlugin可以找出他们并删除重复的依赖
+    new webpack.optimize.OccurrenceOrderPlugin(),         //
+    new webpack.optimize.CommonsChunkPlugin('common.js'), // 提取多个入口文件的公共脚本部分，然后生成一个 common.js 来方便多页面之间进行复用
+    new webpack.optimize.UglifyJsPlugin()                 // 解析/压缩/美化所有的js chunk
+  ] : [],
+  node: {
+    __filename: false,
+    __dirname: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: { presets: ['react', 'es2015'] }
+      }, {
+        test: /.json$/, loader: 'json-loader'
+      }, {
+        test: /.node$/, loader: 'node-loader'
+      }]
+  }
+};
